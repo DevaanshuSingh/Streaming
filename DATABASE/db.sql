aafd72f27320bc29bfc4afce152e1e108c5330d5
DevaanshuSingh <devaanshu111@gmail.com>
2025-03-20 17:56:04 +0530

    Some More,

--- DATABASE/db.sql
+++ DATABASE/db.sql
@@ -2,33 +2,24 @@ CREATE DATABASE stream_db;
 
 use stream_db;
 
-
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `userRegSeriolNo` bigint(20) NOT NULL,
-  `userName` varchar(255) NOT NULL,
-  `fullName` varchar(255) DEFAULT NULL,
-  `userPassword` varchar(255) DEFAULT NULL,
-  `bio` text DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL,
-  `dob` date DEFAULT NULL,
-  `interests` text DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `profile_picture` blob DEFAULT NULL,
+  `user_pass` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `myfeatures` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `featureName` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
 
-CREATE TABLE `myfeatures` (
+
+CREATE TABLE `data` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `featureName` varchar(255) NOT NULL,
+  `fromUserId` bigint(20) NOT NULL,
+  `data` blob DEFAULT NULL,
+  `toUserId` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 
 
 INSERT INTO user (id, user_pass, name) VALUES 
